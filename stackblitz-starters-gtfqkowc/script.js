22463fe145fb717b9f509f44cc014be448f43ecf
elizavetamelcina <elizavetamelcina@gmail.com>
2025-03-05 14:59:26 +0300

    Delete stackblitz-starters-gtfqkowc directory

--- stackblitz-starters-gtfqkowc/script.js
+++
@@ -1,6 +0,0 @@
-animateButton.addEventListener('click', () => {
-  box.classList.add('animate');
-  setTimeout(() => {
-    box.classList.remove('animate');
-  }, 2000); // Длительность анимации
-});
